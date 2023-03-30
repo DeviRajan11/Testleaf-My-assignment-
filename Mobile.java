694c1df1072369f75153efc095696e0bdedd3458
DeviRajan11 <54494020+DeviRajan11@users.noreply.github.com>
2023-03-30 11:09:08 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,32 @@
+package weedk1day1;
+
+public class Mobile {
+	
+	public void sendSms()
+	{
+		System.out.println("From Oppo");
+		
+	}
+	public void makecall()
+	
+	{
+	System.out.println("Calling My Friend");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Mobile action = new Mobile();
+      
+      action.sendSms();
+      action.makecall();
+      
+      
+      
+	}
+
+}
+	
+	
+	
+
+
