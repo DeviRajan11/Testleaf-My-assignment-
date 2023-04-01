8a744b360a174d73131bb63e59c43f1c7c2b4dc4
DeviRajan11 <54494020+DeviRajan11@users.noreply.github.com>
2023-04-01 18:29:23 +0530

    Add files via upload

---
+++ CheckIfTheNumberIsPositiveOrNegative.java
@@ -0,0 +1,25 @@
+package week1day2;
+
+public class CheckIfTheNumberIsPositiveOrNegative {
+
+	public static void main(String[] args) {
+		
+		int num= 0;
+	    
+	    if(num>0)
+	    {
+	    	System.out.println("The number is positive");
+	    }
+	    else if(num<0)
+	    {
+	    System.out.println("The number is Negative");
+	    }
+	    else if(num==0)
+	    {
+	    	System.out.println("The number is neither negative nor positive");
+	    }
+	    }
+		
+	}
+
+
