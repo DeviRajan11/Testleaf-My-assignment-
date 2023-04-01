4c2312669b83093788902731c8fc11100770f5ac
DeviRajan11 <54494020+DeviRajan11@users.noreply.github.com>
2023-04-01 19:48:19 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,24 @@
+package week1day2;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		String mobileBrandName= "Realme";
+		char mobileLogo='@';
+		short noOfMobilePiece=235;
+		int modelNumber= 998745612;
+		long mobileImeiNumber= 4567981314564l;
+		float mobilePrice=14999.89f;
+		boolean isDamaged=false;
+		System.out.println("Brandname = "+mobileBrandName);
+		System.out.println("Logo = "+mobileLogo);
+		System.out.println("Number of pieces =  "+noOfMobilePiece);
+		System.out.println("ModelNumber = "+modelNumber);
+		System.out.println("IMEI number = "+mobileImeiNumber);
+		System.out.println("Price = "+mobilePrice);
+		System.out.println("Isdamaged? "+isDamaged);
+		
+
+	}
+
+}
