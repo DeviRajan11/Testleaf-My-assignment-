1875b053c17153331407eb6c01f64c390be18827
DeviRajan11 <54494020+DeviRajan11@users.noreply.github.com>
2023-04-01 19:38:16 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+package week1day2;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		int n=5;
+		int fact= 1; int i;
+		for (i=1;i<=n;i++)
+		{
+			fact=fact*i;
+			
+		}
+		System.out.println(fact);
+		
+		}
+	}
+
+	
+
