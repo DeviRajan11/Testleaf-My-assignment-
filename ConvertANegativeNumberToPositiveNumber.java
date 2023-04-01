8b66a685750f2cabe0fd02c298a39be662ac909f
DeviRajan11 <54494020+DeviRajan11@users.noreply.github.com>
2023-04-01 18:34:02 +0530

    Add files via upload

---
+++ ConvertANegativeNumberToPositiveNumber.java
@@ -0,0 +1,15 @@
+package week1day2;
+
+public class ConvertANegativeNumberToPositiveNumber {
+
+	public static void main(String[] args) {
+		int num= -60;
+		
+		if(num<0)
+		{
+		int i = num*-1;
+		System.out.println(i);
+		}
+	}
+
+}
